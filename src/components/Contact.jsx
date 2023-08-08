01b8f3ff47845ca6b873d4ec2f32d4d364209311
“Kabir <kabir.thakur416@gmail.com>
2023-08-08 04:06:22 -0400

    some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_nxk84yr',
+        'template_sud0btr',
         {
           from_name: form.name,
           to_name: "Kabir Thakur",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "kabir.thakur416@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'QhJXEidDnGkZLnBbs'
       )
       .then(
         () => {
