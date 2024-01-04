5a6a45be6bd5cca9c150ac307b31e3f05f7535b8
“Kabir <kabir.thakur416@gmail.com>
2024-01-04 12:31:22 -0500

    added NavBar for project categories

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
@@ -20,89 +20,102 @@ const ProjectCard = ({
   links,
 }) => {
   return (
-    // <motion.div variants={fadeIn("up", "spring", index * 0.1, 0.75)} className='flex sm:flex-row flex-col w-full p-4'>
-      <Tilt
-        options={{
-          max: 20,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary p-10 rounded-2xl w-full shadow-card flex sm:flex-row flex-col mb-8'
-      >
-        <div className='relative w-full sm:w-1/2 h-[300px] '>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-fill rounded-2xl'
-          />
-
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-            >
-              <img
-                src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
-              />
-            </div>
+    <Tilt
+      options={{
+        max: 20,
+        scale: 1,
+        speed: 450,
+      }}
+      className='bg-tertiary p-10 rounded-2xl w-full p-4 sm:p-6 md:p-8 lg:p-10 shadow-card flex sm:flex-row flex-col mb-8'
+    >
+      <div className='relative w-full sm:w-1/2 h-[300px]'>
+        <img
+          src={image}
+          alt='project_image'
+          className='w-full h-full object-fill rounded-2xl'
+        />
+        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+          >
+            <img
+              src={github}
+              alt='source code'
+              className='w-1/2 h-1/2 object-contain'
+            />
           </div>
         </div>
-
-        <div className='mt-5 sm:mt-0 sm:ml-5 sm:w-1/2'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{category}</p>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-          <p className='mt-2 text-secondary text-[14px]'>Topics : {topics}</p>
-          <p className='mt-2 text-secondary text-[14px]'>{`${libraries}`}</p>
-          <p className='mt-2'>
-            Links :{" "}
-            <a href={links.l1} target="_blank" rel="noreferrer">
-              {link_labels.label1}
-            </a>
-            &nbsp; {" "}
-            <a href={links.l2} target="_blank" rel="noreferrer">
-              {link_labels.label2}
-            </a>
-          </p>
-          
-        </div>
-      </Tilt>
-    // </motion.div>
+      </div>
+      <div className='mt-5 sm:mt-0 sm:ml-5 sm:w-1/2'>
+        <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+        {/* <p className='mt-2 text-secondary text-[14px]'>{category}</p> */}
+        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        <p className='mt-2 text-secondary text-[14px]'>Topics : {topics}</p>
+        <p className='mt-2 text-secondary text-[14px]'>{libraries}</p>
+        <p className='mt-2'>
+          Links :  &nbsp;
+          <a href={links.l1} target="_blank" rel="noreferrer">
+            {link_labels.label1}
+          </a>
+          {links.l2 && // If links.l2 is available, render the comma and the link
+            <>
+              ,&nbsp;
+              <a href={links.l2} target="_blank" rel="noreferrer">
+                {link_labels.label2}
+              </a>
+            </>
+          }
+        </p>
+      </div>
+    </Tilt>
   );
 };
 
 const Works = () => {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const allCategories = projects.flatMap(project => project.category);
+  const uniqueCategories = ['All', ...new Set(allCategories)];
+
+  const filteredProjects = selectedCategory === 'All'
+    ? projects
+    : projects.filter(project => project.category.includes(selectedCategory));
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      {/* <motion.div variants={textVariant()}> */}
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      {/* </motion.div> */}
+      <p className={`${styles.sectionSubText}`}>My work</p>
+      <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
 
       <div className='w-full flex flex-col items-center'>
-        {/* <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
-        > */}
-          Following projects showcases my skills and experience through
+        <p className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+          Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
-        {/* </motion.p> */}
-
-        
+        </p>
+      </div>
+    <br></br>
+      <div className="bg-tertiary rounded-2xl p-4 flex justify-center space-x-4">
+        {uniqueCategories.map(category => (
+          <button
+            key={category}
+            className={`text-white ${selectedCategory === category ? 'font-bold' : ''}`}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
       </div>
 
       <div className='mt-20 flex flex-wrap justify-center'>
-            {projects.map((project, index) => (
+        {filteredProjects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
     </div>
   );
 };
+
 export default SectionWrapper(Works, "projects");
--- src/constants/index.js
+++ src/constants/index.js
@@ -49,7 +49,7 @@ import {
 
     {
       name: "HealthCost Insight: A Data-Driven Approach to Healthcare Cost Management",
-      category: "Data Science",
+      category: ["Data Science"],
       description:
         "In this data science project, we analyzed healthcare cost data to identify key influencing factors. We used statistical modeling to understand the relationships within the data and made strategic recommendations for cost reduction based on our findings. To enhance the accessibility of our results, we developed an interactive dashboard using Shiny App in R, allowing users to visualize the data and test different models. This project showcases our ability to derive actionable insights from data and contribute to effective healthcare cost management.",
       topics: "Exploratory Data Analysis, Data Preprocessing and Cleaning, Correlation Analysis, Univariate Analysis, Bivariate Analysis, Multivariate Analysis, Linear Regression, Multiple Linear Regression, Model Evaluation (R-squared values), Data Visualization (Scatterplots, Maps), Predictive Modeling, Shiny App Development, Dashboard Creation, Data-Driven Recommendations, Model Building (Tree Bag, Support Vector Machine), Model Confusion Analysis, Data Insights Interpretation, R Programming, Statistical Analysis, Healthcare Cost Analysis",
@@ -64,7 +64,7 @@ import {
     },
     {
       "name": "SkillSpotter: Mining Skills from Job Descriptions using NER",
-      "category": "NLP, NER, NLI, Transformers, BERT" ,
+      "category": ["Data Science","NLP","Deep Learning"] ,
       "description":"This paper presents ”SkillSpotter”, a novel approach to extracting and categorizing skills from job descriptions using advanced text mining techniques. Utilizing Named Entity Recognition (NER), the study leverages a fine-tuned BERT model that accurately identifies both general and specific technological and soft skills from a vast dataset of web-scraped job descriptions. The project addresses challenges in data sampling, annotation, and model optimization while considering the ethical implications of data usage and technology in human resource contexts. The findings offer valuable insights into job market trends and skill demands, highlighting the potential of NER in job recommendation systems.",
       "topics": "Named Entity Recognition, BERT, Text Mining, Machine Learning, Data Cleaning, Data Sampling, Model Optimization, Job Market Trends Analysis, Skill Demand Analysis, Recommendation Systems, NLU, NLI, Data Transformation, Model Training and Inference, Skill Taxonomy Development, Data Annotation, Pattern Matching",
       "libraries": "Python Libraries: PyTorch, transformers, spacy, NLTK, Pandas, datasets, seqeval, BeautifulSoup 4",
@@ -78,7 +78,7 @@ import {
     },
     {
       "name": "Experiments In Neural Style Transfer",
-      "category": "Neural Style Transfer (NST), VGG19, AlexNet, Transfer Learning" ,
+      "category": ["Deep Learning"] ,
       "description":"This paper explores the intersection of artificial intelligence and art by implementing and experimenting with Neural Style Transfer (NST) algorithms. The objective is to blend the content of one image with the artistic style of another, creating unique art pieces. It involves an in-depth investigation of various optimizers like Adam and L-BFGS and pre-trained neural network models, such as VGG19 and AlexNet, to evaluate their impact on NST outcomes. The aim of this paper is to explore the underlying mathematics of NST by manipulating the loss function and experimenting with hyperparameters to create a spectrum of stylized images. Through this project, the authors seek to understand how different neural network architectures and NST parameters contribute to the art generation process, with the ultimate goal of achieving a nuanced understanding of NST in artistic creation.",
       "topics": "Neural Style Transfer (NST), PyTorch, Adam Optimizer, L-BFGS Optimizer, VGG19, AlexNet",
       "libraries": "Python Libraries: PyTorch, torchvision, ",
@@ -92,9 +92,9 @@ import {
     },
     {
       "name": "Gomoku Using a Value Neural Network",
-      "category": "Value Network, AI, CNN, GOMOKU, Heuristic Analysis" ,
+      "category": ["Deep Learning"] ,
       "description":"This study introduces GomokuAI, an artificial intelligence designed to play the board game Gomoku. The AI combines hard-coded strategies with a neural network-based heuristic approach. Developed using a dataset from 2000 simulated games of minimax algorithm with alpha beta pruning playing against itself, the AI prioritizes winning moves, blocks opponent wins, and strategically responds to various game situations. The neural network, implemented using PyTorch, enhances the AI's decision-making capabilities. Initial results indicate that GomokuAI significantly outperforms minimax, achieving a win or draw rate of 60-70%. The paper suggests future improvements could include further model optimization and integration with advanced algorithms like Monte Carlo Tree Search. This research demonstrates the potential of combining traditional game strategies with machine learning techniques in game AI development.",
-      "topics": "AI, Neural Networks, CNN, Monte Carlo Tree Search",
+      "topics": "AI, Neural Networks, CNN, Monte Carlo Tree Search, Value Network, AI, CNN, GOMOKU, Heuristic Analysis",
       "libraries": "Python Libraries: PyTorch ",
       "link_labels": { "label1": "Project Repository", "label2": "Report" },
       "links": {
@@ -106,7 +106,7 @@ import {
     },
     {
       name: "Dynamic Human AI Collaboration",
-      category: "Machine Learning",
+      category: ["Machine Learning","Data Science"],
       description: "In this project, I worked with the Decision Science team at Jaypee Morgan London to proposed a Bayesian framework for human-in-the-loop pipelines, where the final decision is a combination of algorithm and expert opinions. We argued that updating expert opinion priors with information sharing between experts is key to achieving superior performance. We utilized the SA Heart dataset to predict whether a patient has a coronary heart disease. We proposed a Bayesian framework for human-in-the-loop systems and leveraged collaborative intelligence. We also showed that deferral systems are a special case where the combining expert opinions follows a categorical distribution.",
       topics: "Human-in-the-loop, Machine Learning, Bayesian Framework, Collaborative Intelligence, Deferral Systems, Expert Opinions, Information Sharing",
       libraries: "Python : PyMC3, sklearn, theano, statsmodels, SciPy, NumPy, Pandas, Matplotlib, Seaborn, arviz",
@@ -119,7 +119,7 @@ import {
     },
     {
       name: "CusineCluster: Restaurant Recommendation System",
-      category: "Machine Learning",
+      category: ["Machine Learning","Data Science"],
       description:
         "This project presents a hybrid recommendation system for restaurants, leveraging K-means clustering and Alternating Least Squares (ALS) based Collaborative Filtering. The system was trained on a dataset of over 8 million individual reviews, segmenting restaurants into six distinct clusters. The resulting model demonstrated high accuracy in predicting user ratings, offering potential for personalized and effective restaurant recommendations.",
       topics: "PySpark, Data Preprocessing, Feature Engineering, K-means Clustering, Collaborative Filtering, ALS, Hybrid Recommendation Engine",
@@ -134,7 +134,7 @@ import {
     },
     {
       name: "StoreSage: Analyzing Supermarket Data for Enhanced Retail Strategies in Myanmar",
-      category: "Data Analysis",
+      category: ["Data Analysis"],
       description: "This project analyzes sales data from three supermarkets in Myanmar, providing insights on store performance, customer experience, and product categories. It identifies opportunities to improve satisfaction, diversify offerings, and optimize strategies for profitability. Valuable guidance for enhancing retail performance is derived from gender and time trends.",
       topics: "Data Cleaning and Preprocessing, Exploratory Data Analysis (EDA), Correlation Analysis, Data Visualization, Statistical Analysis, Jupyter Notebook, GitHub",
       libraries: "Python Libraries : Pandas, Matplotlib, Seaborn",
@@ -148,7 +148,7 @@ import {
     },
     {
       name: "Banking Behaviors : An Association Rule Expedition",
-      category: "Machine Learning",
+      category: ["Machine Learning","Data Science"],
       description:
         "This project analyzes sales data from three supermarkets in Myanmar, providing insights on store performance, customer experience, and product categories. It identifies opportunities to improve satisfaction, diversify offerings, and optimize strategies for profitability. Valuable guidance for enhancing retail performance is derived from gender and time trends.",
       topics: " One Hot Encoding, Data Cleaning, Data Visualization, Discretization, Data Segmentation, Association Rule Mining",
@@ -163,7 +163,7 @@ import {
     },
     {
       name: "MailSift: NLP-Driven Spam Detection",
-      category: "Natural Language Processing",
+      category: ["NLP"],
       description:
         "In this NLP project, we analyzed the Enron public email corpus to build classifiers for identifying spam emails. We utilized the Natural Language Tool Kit (NLTK) library in Python to tokenize emails and extract features such as unigrams, bigrams, trigrams, and part-of-speech tags. We also used a subjectivity lexicon to count the number of positive and negative words in the emails. These features were then used to train a Naive Bayes Classifier and a Support Vector Machine (SVM). The performance of the classifiers was evaluated using measures such as accuracy, precision, recall, and F1 score. This project demonstrates our ability to apply NLP techniques and machine learning to develop effective spam filters.",
       topics: "Natural Language Processing, Text Tokenization, Feature Extraction, Unigram Features, Bigram Features, Trigram Features, Part-of-Speech Tagging, Subjectivity Lexicon, Naive Bayes Classifier, Support Vector Machine, Model Evaluation (Accuracy, Precision, Recall, F1 Score), Spam Email Classification",
@@ -178,7 +178,7 @@ import {
     },
     {
       name: "Quantum Wave Function Simulation",
-      category: "Computational Physics",
+      category: ["Computational Physics"],
       description: "This project simulates the time evolution of a quantum wave function as it interacts with various potential barriers. The wave function is initially a Gaussian wave packet, and the potential can be a single barrier, multiple barriers, or a step potential. The simulation is visualized using matplotlib's animation function.",
       topics: "Quantum Mechanics, Wave Function, Schrodinger Equation, Computational Physics",
       libraries: "Python Libraries : numpy, matplotlib",
@@ -191,7 +191,7 @@ import {
     },
     {
       name: "Phase Transformations in Metallic Alloys",
-      category: "Computational Physics",
+      category: ["Computational Physics"],
       description: "This project uses the Finite Difference Method and the Spectral Method, along with the Euler Explicit method, to study phase transformations in metallic alloys by solving the Cahn-Hilliard equation. The theoretical basis for thermodynamic models is discussed, and a modified version of Fick's second law of diffusion is used to account for energy related to interfaces. The methods are implemented and optimized in MATLAB, and used to simulate microstructures due to phase separation caused by various initial composition profiles.",
       topics: "Phase Transformations, Metallic Alloys, Cahn-Hilliard Equation, Finite Difference Method, Spectral Method, Euler Explicit Method, Thermodynamic Models, Fick's Second Law of Diffusion.",
       libraries: "MATLAB",
